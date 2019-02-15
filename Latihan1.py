c1e90feba266d625760392c2d9ffeb8cc78816c8
Zaenal Mustofa <zaenalmusthofa97@gmail.com>
2019-02-16 00:52:44 +0700

    Program menampilkan n bilangan acak lebih < 0.5

---
+++ Latihan1.py
@@ -0,0 +1,13 @@
+print('|-----------------------------------------------|')
+print('|PROGRAM MENAMPILKAN N BILANGAN ACAK LEBIH < 0.5|')
+print('|-----------------------------------------------|')
+print('')
+
+import random
+
+n = int(input("Masukan nilai N : "))
+for i in range(n) :
+    a=random.uniform(0.0,0.5)
+    print ("Data ke : ", i, "=> ", a)
+    
+print("Selesai")
