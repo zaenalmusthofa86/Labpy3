744a78629a31de13a8ec989549779da572c33ad8
Zaenal Mustofa <zaenalmusthofa97@gmail.com>
2019-02-16 00:54:28 +0700

    Program menampilkan bilangan terbesar

---
+++ Latihan2.py
@@ -0,0 +1,16 @@
+print ('|-------------------------------------|')
+print ('|PROGRAM MENAMPILKAN BILANGAN TERBESAR|')
+print ('|-------------------------------------|')
+print ('')
+
+a=1
+max=0
+while a!=0:
+    if a>max:
+        max=a
+    a=int(input('Masukkan bilangan :'))
+    if a==0:
+         break
+        
+print ('Nilai terbesarya adalah :',max)
+   
